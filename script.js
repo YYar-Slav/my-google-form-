7b14540f21d53c7a685940021604624eaec2cc3b
YYar-Slav <aroslavpalha@gmail.com>
2026-03-26 22:06:10 +0200

    Refactor login function for clarity and comments

--- script.js
+++ script.js
@@ -7,12 +7,11 @@ function login() {
         return;
     }
 
+    // Замініть URL на свій Web App URL
     fetch("https://script.google.com/macros/s/AKfycbwcpN5A7o4hbxuWYgy77K35utt5zm5YRW9DPXypMAbgw2oy65aAbpH75ls_cqeqVs6GCQ/exec", {
         method: "POST",
         body: JSON.stringify({ username: user, password: pass }),
-        headers: {
-            "Content-Type": "application/json"
-        }
+        headers: { "Content-Type": "application/json" }
     })
     .then(response => response.json())
     .then(result => {
